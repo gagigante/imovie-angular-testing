1b7d50b48d05ceec0de79224a092af86b25c5481
Gabriel Gigante <gabriel.gigante@accountfy.com>
2021-05-16 10:28:12 -0300

    feat: setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
