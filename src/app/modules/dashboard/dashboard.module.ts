97f94b1277bac51011b385bb17a83000b9985c9b
Gabriel Gigante <gabriel.gigante@accountfy.com>
2021-05-16 10:46:34 -0300

    feat: setup angular material

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatIconModule } from '@angular/material';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, MenuComponent],
   imports: [
     CommonModule,
+    MatIconModule,
     DashboardRoutingModule
   ]
 })
